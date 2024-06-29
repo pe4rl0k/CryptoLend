df7bd4a8d488b1d4cd2dd4e02d443cb92f658d9b
pe4rl0k <163330893+pe4rl0k@users.noreply.github.com>
2024-06-29 12:42:59 +0200

    change variable names

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-import loans
+import loans_module as loans_service
 
 load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/status', methods=['GET'])
-def check_status():
+def check_server_status():
     return {"status": "Server is running!"}
 
 @app.route('/loans', methods=['GET'])
-def get_loans():
-    return loans.get_loans()
+def fetch_all_loans():
+    return loans_service.get_loans_list()
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    server_port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=server_opt)
