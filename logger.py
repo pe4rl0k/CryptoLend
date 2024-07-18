5c6cab878acf27b07807fa28ef9dbbbeae9650df
pe4rl0k <163330893+pe4rl0k@users.noreply.github.com>
2024-07-18 17:09:30 +0200

    improve readability

--- logger.py
+++ logger.py
@@ -6,20 +6,20 @@ load_dotenv()
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
 LOG_FORMAT = os.getenv('LOG_FORMAT', '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-LOG_DATEFMT = os.getenv('LOG_DATEFMT', '%Y-%m-%d %H:%M:%S')
-LOG_FILENAME = os.getenv('LOG_FILENAME', 'cryptolend.log')
+LOG_DATE_FORMAT = os.getenv('LOG_DATEFMT', '%Y-%m-%d %H:%M:%S')
+LOG_FILE_NAME = os.getenv('LOG_FILENAME', 'cryptolend.log')
 
 logger = logging.getLogger('CryptoLendLogger')
 logger.setLevel(LOG_LEVEL)
 
 if not logger.handlers:
-    file_handler = logging.FileHandle(LOG_filename)
+    file_handler = logging.FileHandler(LOG_FILE_NAME)
     stream_handler = logging.StreamHandler()
 
     file_handler.setLevel(LOG_LEVEL)
     stream_handler.setLevel(LOG_LEVEL)
 
-    formatter = logging.Formatter(LOG_FORMAT, datefmt=LOG_DATEFMT)
+    formatter = logging.Formatter(LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
     file_handler.setFormatter(formatter)
     stream_handler.setFormatter(formatter)
 
