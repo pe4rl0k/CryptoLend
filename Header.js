99968b4727f54ff94f64b919b6c8ec261f6a40b3
pe4rl0k <163330893+pe4rl0k@users.noreply.github.com>
2024-08-18 10:56:53 +0200

    improve readability

--- Header.js
+++ Header.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+const Navigation = () => (
+  <nav className="navigation">
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/market">Market</Link></li>
+      <li><Link to="/lend">Lend</Link></li>
+      <li><Link to="/borrow">Borrow</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+  </nav>
+);
+
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">CryptoLend</Link>
       </div>
-      <nav className="navigation">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/market">Market</Link></li>
-          <li><Link to="/lend">Lend</Link></li>
-          <li><Link to="/borrow">Borrow</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
+      <Navigation />
     </header>
   );
 };
